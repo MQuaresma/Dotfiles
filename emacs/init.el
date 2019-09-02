4c1ab82961415e6e5406024f24e4f639749ea1bb
MQuaresma <miguelquaresma97@gmail.com>
2019-09-02 08:08:18 +0100

    Clean emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -1,21 +1,18 @@
-(cua-mode)
-(setq mac-option-modifier 'none)
-(setq-default
- c-default-style "linux"
- c-basic-offset 4
- tab-width 4
- indent-tabs-mode nil)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(global-hl-line-mode 1)
+(global-display-line-numbers-mode)
 
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives
-               (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+;; Repositories
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa", "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
-
-(load-file "~/.emacs.d/elpa/proof-general-20190606.933/generic/proof-site.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -23,10 +20,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cua-mode t nil (cua-base))
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(easycrypt-prog-name "/Users/miguelq/.opam/easycrypt/bin/easycrypt")
  '(package-selected-packages
    (quote
-    (dtrace-script-mode auctex-latexmk company-coq auctex proof-general))))
+    (neotree spacemacs-theme dtrace-script-mode auctex-latexmk company-coq auctex proof-general))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,9 +35,25 @@
  ;; If there is more than one, they won't work right.
  )
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(global-display-line-numbers-mode)
-(setq TeX-PDF-mode t)
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(load-theme 'spacemacs-dark)
+(load-file "/Users/miguelq/.opam/easycrypt/share/proofgeneral/generic/proof-site.el")
+
+(add-to-list 'load-path "/Users/miguelq/.emacs.d/elpa/neotree-20181121.2026")
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(cua-mode)
+(setq mac-option-modifier 'none)
+
+(setq-default
+ c-default-style "linux"
+ c-basic-offset 4
+ tab-width 4
+ indent-tabs-mode nil)
+
 (add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.jazz\\'" . c-mode))
+
+(setq TeX-PDF-mode t)
